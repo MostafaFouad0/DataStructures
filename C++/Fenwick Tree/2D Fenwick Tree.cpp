4895a756e32e429e7e4e52619e5c325a20aa296e
Mostafa Mahmoud Fouad <mm1471800@gmail.com>
2022-12-16 02:59:45 +0200

    Create 2D Fenwick Tree.cpp

---
+++ C++/Fenwick Tree/2D Fenwick Tree.cpp
@@ -0,0 +1,37 @@
+struct BIT {
+    ll n{}, m{};
+    ll bit[N][N] = {0};
+
+    ll LSB(ll num) {
+        return num & -num;
+    }
+
+    void update(ll x, ll y, ll val) {
+        while (x <= n) {
+            ll tmp = y;
+            while (y <= m) {
+                bit[x][y] += val;
+                y += LSB(y);
+            }
+            y = tmp;
+            x += LSB(x);
+        }
+    }
+
+    ll query(ll x, ll y) {
+        ll ret{};
+
+        while (x > 0) {
+            ll tmp = y;
+            while (y > 0) {
+                ret += bit[x][y];
+                y -= LSB(y);
+            }
+            y = tmp;
+            x -= LSB(x);
+        }
+        return ret;
+    }
+
+
+} 
